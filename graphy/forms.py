8c2148a67e6fc40661684b172da16e325d0542f0
Nilay Patel <nilay.patel@ada.ac.uk>
2018-10-16 17:56:44 +0100

    changed the way the post request is handelled. Changes to some renders in index

--- graphy/forms.py
+++ graphy/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 
+YEAR_CHOICES= [
+    ('Completions_2014', '2014'),
+    ('Completions_2015', '2015'),
+    ]
+
 class ProviderName(forms.Form):
     provider_name = forms.CharField()
     second_provider_name = forms.CharField()
+    #Completion year
+    first_completion_year = forms.CharField(label='What completion year?', widget=forms.Select(choices=YEAR_CHOICES))
+    second_completion_year = forms.CharField(label='What completion year?', widget=forms.Select(choices=YEAR_CHOICES))
+
+# class CompletionYear(forms.Form):
--- graphy/views.py
+++ graphy/views.py
@@ -1,49 +1,61 @@
 import csv
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from graphy.forms import ProviderName
+import re
+# from graphy.forms import CompletionYear
 
 
 # Create your views here.
 
 
-
+# read the csv and append it to a list
 def read_csv():
     data = []
     datafile = open('PR0102.csv', 'r')
     datareader = csv.reader(datafile)
-
+    # for loop reads data in csv by row and puts it into a list
     for row in datareader:
         data.append(row)
 
     datafile.close()
     return data
 
+#lsit of csv data
 data = read_csv()
 
+# form request
 def index(request):
-    form = ProviderName()
-    selected_data = []
-    second_data = []
+    provider_form = ProviderName()
 
+    selected_data = None
+    #checks if the post request is valid, if it is not valid then a request 404 page is displayed.
     if request.method == 'POST':
         selected_provider_name = request.POST.get('provider_name')
         second_provider_name = request.POST.get('second_provider_name')
+
+        selected_data = {selected_provider_name: [], second_provider_name: []}
+
+        # Not yet implemented
+        first_completion_year = request.POST.get('first_completion_year')
+        second_completion_year = request.POST.get('second_completion_year')
+
+
         # if selected_provider_name == None:
-        #     return HttpResponse_404
-
-        print(selected_provider_name)
-        print(second_provider_name)
+        #     return HttpResponseNotFound('<h1>Page not found</h1>')
 
 
-        for colIndex in range(len(data)):
-            if data[colIndex][1] == selected_provider_name:
-                selected_data.append(data[colIndex])
-            elif data[colIndex][0] == second_provider_name:
-                second_data.append(data[colIndex])
-            elif data[colIndex][1] == second_provider_name:
-                second_data.append(data[colIndex])
+
+        for row in data:
+            if row[3] == 'All' and row[1] in [selected_provider_name, second_provider_name]:
+                selected_data[row[1]].append(row)
+            elif row[3] == 'All' and row[0] in [selected_provider_name, second_provider_name]:
+                selected_data[row[0]].append(row)
+
+        # for row in data:
+        #     if data[5][0] ==
 
 
-                #print(colIndex)
-    return render(request, 'index.html', {'data': selected_data, 'second_data': second_data, 'form': form})
+
+    return render(request, 'index.html', {'selected_data': selected_data,
+        'provider_form': provider_form})
